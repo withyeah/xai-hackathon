c2131704c8c4d792ab1956470797b166feeafbfa
Alex Gim <areygim@gmail.com>
2020-03-20 17:18:08 +0900

    static

--- hack/settings.py
+++ hack/settings.py
@@ -121,4 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
+# static files
 STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
